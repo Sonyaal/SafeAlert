3723c67851d4e6da6a07175b0ac3c495dc76e08f
Sonya Alexis <salexis@usc.edu>
2024-12-02 02:02:50 -0800

    adding encryption script

--- encryption_script.py
+++ encryption_script.py
@@ -1,10 +1,12 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 # Generate private and public keys
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
+    backend=default_backend()
 )
 
 # Serialize and save the private key
